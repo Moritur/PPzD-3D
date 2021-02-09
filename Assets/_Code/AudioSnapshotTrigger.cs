017d055a07b873a45c1c1eb1627a9ad6bebc6709
Moritur <44618240+Moritur@users.noreply.github.com>
2021-02-09 22:03:38 +0100

    Add sound to scenes

---
+++ Assets/_Code/AudioSnapshotTrigger.cs
@@ -0,0 +1,34 @@
+﻿using FMOD.Studio;
+using FMODUnity;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioSnapshotTrigger : MonoBehaviour
+{
+    #region set in editor
+    [Tooltip("Name of the VCA which should be disabled when wntering this trigger")]
+    [SerializeField] string disableVcaName;
+    [Tooltip("Name of the VCA which should be enabled when wntering this trigger")]
+    [SerializeField] string enableVcaName;
+    #endregion
+
+    VCA disableVca;
+    VCA enableVca;
+
+    void Awake()
+    {
+        disableVca = RuntimeManager.GetVCA(disableVcaName);
+        enableVca = RuntimeManager.GetVCA(enableVcaName);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            disableVca.setVolume(0f);
+            enableVca.setVolume(1f);
+        }
+    }
+}
---
+++ Assets/_Code/InitialVcaSetup.cs
@@ -0,0 +1,20 @@
+﻿using FMODUnity;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitialVcaSetup : MonoBehaviour
+{
+    #region set in editor
+    [Tooltip("Name of VCA which should be initially disabled")]
+    [SerializeField] string enableVcaName;
+    [Tooltip("Name of VCA which should be initially enabled")]
+    [SerializeField] string disableVcaName;
+    #endregion
+
+    void Awake()
+    {
+        RuntimeManager.GetVCA(disableVcaName).setVolume(0f);
+        RuntimeManager.GetVCA(enableVcaName).setVolume(1f);
+    }
+}
